0f81da4a0e71e245020797c0de25d67160531c15
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2022-12-31 09:02:48 +0300

    three

--- brain_games/logic_game.py
+++ brain_games/logic_game.py
@@ -3,7 +3,7 @@ import prompt
 
 def game_start(game):
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
+    name = prompt.string('May I have yor name? ')
     print(f'Hello, {name}!')
     print(game.GAME)
 
