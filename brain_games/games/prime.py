dfdd4ec12090b855e91e56868c46b0961646fa4c
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2022-12-31 09:26:57 +0300

    prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-GAME = 'Answer "yes" if given number is prime. Otherwisr answer "no".'
+GAME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 MIN_NUM = 1
