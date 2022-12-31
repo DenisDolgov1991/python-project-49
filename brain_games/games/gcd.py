ddbdc55cbdd3e70696970a256296c9eb690e05ab
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2022-12-31 09:13:39 +0300

    gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -12,6 +12,6 @@ MAX_NUM = 50
 def brain_ring():
     num1 = randint(MIN_NUM, MAX_NUM)
     num2 = randint(MIN_NUM, MAX_NUM)
-    question = f'{num1} and {num2}'
+    question = f'{num1} {num2}'
     correct_answer = str(math.gcd(num1, num2))
     return question, correct_answer
