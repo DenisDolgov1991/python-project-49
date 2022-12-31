364251786018d5bec174cc8b50bb1d93967bde57
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2022-12-31 09:39:26 +0300

    prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -21,7 +21,7 @@ def brain_ring():
 def is_prime(num):
     if num <= 1:
         return False
-    for i in range(2, numr):
+    for i in range(2, num):
         if num % i == 0:
             return False
     return True
